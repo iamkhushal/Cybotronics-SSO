e235fa6cf4de095d3ab164e4ae9bcc7e24f886de
Khushal <khushal@internlabs.io>
2023-01-23 23:36:09 +0530

    Firebase Push Notification Configuration

--- Cybotronics-SSO/BaseClasses/AppDelegate.swift
+++ Cybotronics-SSO/BaseClasses/AppDelegate.swift
@@ -1,12 +1,8 @@
-//
-//  AppDelegate.swift
-//  Cybotronics-SSO
-//
-//  Created by Demo User on 21/01/23.
-//
-
 import UIKit
 import CoreData
+import FirebaseCore
+import FirebaseMessaging
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +10,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+
+        registerForPushNotifications()
         return true
+    }
+
+    private func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+
+
+                UIApplication.shared.registerForRemoteNotifications()
+            }
     }
 
     // MARK: UISceneSession Lifecycle
@@ -52,3 +61,59 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate : UNUserNotificationCenterDelegate{
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // print("Device Token ===> \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    // Receive displayed notifications for iOS 10 devices.
+    //Called when app in forground
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo["aps"] as? NSDictionary
+        print(userInfo)
+         print(notification.request.content.userInfo)
+    }
+
+
+    //Called when app in Killed and reopen
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        print(userInfo)
+        completionHandler()
+    }
+}
+
+extension AppDelegate : MessagingDelegate{
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        let token = Messaging.messaging().fcmToken
+        guard let fcmToken = token else { return }
+
+        let dataDict:[String: String] = ["token": fcmToken ]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        print("Firebase registration token: \(String(describing: fcmToken))")
+
+        UserDefaults.standard.set(token, forKey: "KeyChain_FCMDeviceToken")
+
+    }
+
+
+
+
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+        print(userInfo)
+    }
+}
